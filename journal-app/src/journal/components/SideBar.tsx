2b2b77816b2e12489434451a8861156c01100b3a
Diego Lezama <diegolezama22@gmail.com>
2025-07-21 23:57:04 -0400

    Add files via upload

---
+++ journal-app/src/journal/components/SideBar.tsx
@@ -0,0 +1,58 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+export const SideBar = ({ drawerWidth = 240 }: { drawerWidth: number }) => {
+  return (
+    <Box
+      component={"nav"}
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { sx: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component={"div"}>
+            Diego Lezama
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText
+                    secondary={
+                      "Ipsum consectetur consequat cillum exercitation commodo veniam."
+                    }
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
